184381bfd43d34298a7d159034e2ecccc3f309b1
EdouardWP <88239973+EdouardWP@users.noreply.github.com>
2024-04-26 16:24:40 +0200

    Update MistralAImodel2.py

--- MistralAImodel2.py
+++ MistralAImodel2.py
@@ -1,46 +1,48 @@
-import os
-import gradio as gr
 from llama_index.llms.mistralai import MistralAI
 from llama_index.embeddings.mistralai import MistralAIEmbedding
 from llama_index.core.settings import Settings
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+import gradio as gr
+from gradio_pdf import PDF
+import os
 
 api_key = os.getenv('MISTRAL_API_KEY')
 if not api_key:
     raise ValueError("API key not found. Please set the MISTRAL_API_KEY environment variable.")
 
-# Initialize the models with the API key
 llm = MistralAI(api_key=api_key, model="open-mistral-7b")
 embed_model = MistralAIEmbedding(model_name='mistral-embed', api_key=api_key)
 
-# Configure global settings with the initialized models
 Settings.llm = llm
 Settings.embed_model = embed_model
 
-def qa(question: str, doc: str) -> str:
-    # Load the PDF document
-    my_pdf = SimpleDirectoryReader(input_files=[doc]).load_data()
-    # Create an index from the document for querying
-    my_pdf_index = VectorStoreIndex.from_documents(my_pdf)
-    # Transform the index into a query engine
-    my_pdf_engine = my_pdf_index.as_query_engine()
-    # Query the document
-    response = my_pdf_engine.query(question)
-    return response    
+def qa(question: str, doc) -> str:
+    if not doc:
+        return "No document uploaded."
+    file_path = doc.name  # Get the temporary file path
 
+    try:
+        my_pdf = SimpleDirectoryReader(input_files=[file_path]).load_data()
+        my_pdf_index = VectorStoreIndex.from_documents(my_pdf)
+        my_pdf_engine = my_pdf_index.as_query_engine()
+        response = my_pdf_engine.query(question)
+        if not response:
+            return "No answer found in the document."
+        return response
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
-
-# Set up the Gradio interface with imported CSS and customizations
 demo = gr.Interface(
     fn=qa,
-    inputs=[gr.File(label="Upload Document"), 
-            gr.Text(label="Question", placeholder="Type your question here")],
-    outputs= gr.Text(label="Memor's Answer", lines=15),
+    inputs=[
+        gr.Text(label="Question", placeholder="Type your question here"), 
+        gr.File(label="Upload Document")
+    ],
+    outputs=gr.Text(label="Memor's Answer", lines=15),
     title="Memorious Tech Demo",
     description="Upload a PDF document and ask a question about its content now, or later.",
-    theme = "soft"
+    theme="soft"
 )
-
 
 if __name__ == "__main__":
     demo.launch()
